ea205f87387f86a4d754fe5bd91252c13eba471a
noammmm <noam.surnmame@gmail.com>
2021-03-05 20:31:59 +0000

    first rectangle done

--- chess.py
+++ chess.py
@@ -1,8 +1,14 @@
 import pygame as pg
 import sys
 pg.init()
-size = 200, 200
+green = (118, 150, 85)
+size = 1000, 1000
 screen = pg.display.set_mode(size)
 while 1:
     for event in pg.event.get():
         if event.type == pg.QUIT: sys.exit()
+    screen.fill(green)
+    pg.display.update()
+    pg.draw.rect(screen, (255, 255, 255), (10, 10, 10 ,10 ))
+    pg.display.update()
+
