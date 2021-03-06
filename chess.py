de11dc4af5e3c1521e3c04cf53213a42783f79dc
louis <poes997@gmail.com>
2021-03-06 01:12:56 +0000

    initialize pieces function

--- chess.py
+++ chess.py
@@ -1,12 +1,68 @@
 import pygame as pg
 import sys
 from helpers import *
+from piece import *
 
 pg.init()
 
 green = (118, 150, 85)
 size = 400, 400
 screen = pg.display.set_mode(size)
+
+p = Pawn((1, 2),"King", "black")
+print(p.colour)
+
+
+
+def initialize_positions():
+	letters = ["A","B","C","D","E","F","G","H"]
+	sc = ["A", "H", "B", "G", "C", "F"]
+	side_pieces = ["Rook", "Knight", "Bishop", "Queen"]
+	colour=None
+	p_type=None
+	squares = []
+	for number in range(1,9):
+		for letter in letters:
+			# set pawns
+			colour = None
+			p_type = None
+			if number==2:
+				colour = "White"
+				p_type = "Pawn"
+			if number==7:
+				colour = "Black"
+				p_type = "Pawn"
+			# back pieces			
+			if number==1:
+				# white back
+				for i in range(0, len(sc), 2):
+					if letter == sc[i] or letter == sc[i+1]:
+						colour = "White"
+						p_type = side_pieces[i//2]
+				if letter=="D":
+					colour = "White"
+					p_type = "Queen"
+				if letter=="E":
+					colour = "White"
+					p_type = "King"
+
+			if number==8:
+				# black back
+				for i in range(0, len(sc), 2):
+					if letter == sc[i] or letter == sc[i+1]:
+						colour = "Black"
+						p_type = side_pieces[i//2]	
+				if letter=="D":
+					colour = "Black"
+					p_type = "Queen"
+				if letter=="E":
+					colour = "Black"
+					p_type = "King"
+
+			squares.append({letter:number, "piece":(colour, p_type)})
+	return squares
+
+print(initialize_positions())
 
 
 def drawchequerboard(n, size):
--- piece.py
+++ piece.py
@@ -1,5 +1,18 @@
-class piece:
-"""This is the class for a chess piece"""
+class Piece:
+	"""This is the class for a chess piece"""
+	def __init__(self, pos, piece_type, colour):
+		self.x = pos[0]
+		self.y = pos[1]
+		self.piece_type = piece_type
+		self.colour = colour
 
-	def __init__(self):
+	def draw():
 		pass
+
+	def move():
+		pass
+
+
+
+class Pawn(Piece):
+	pass
